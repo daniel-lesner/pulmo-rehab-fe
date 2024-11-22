3d81b71bb29e35580779dbd0a1c8af68f10bc233
Daniel Lesner <daniel.lesner@icloud.com>
2024-11-22 14:36:39 +0100

    Add ember-cp-validations and setup bootstrap-icons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,5 +7,13 @@ module.exports = function (defaults) {
     // Add options here
   });
 
+  app.import('node_modules/bootstrap-icons/font/bootstrap-icons.css');
+  app.import('node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff', {
+    destDir: 'assets/fonts',
+  });
+  app.import('node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2', {
+    destDir: 'assets/fonts',
+  });
+
   return app.toTree();
 };
