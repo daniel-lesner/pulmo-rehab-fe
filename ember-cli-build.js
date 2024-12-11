85681b54383a6bedaae51c756f24ff7c3d8da62a
Daniel Lesner <daniel.lesner@icloud.com>
2024-12-11 19:43:30 +0100

    Install ember-boostrap and ember-bootstrap-icons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,16 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      bootstrapVersion: 5,
+      importBootstrapCSS: false,
+      insertEmberWormholeElementToDom: false,
+    },
   });
 
-  app.import('node_modules/bootstrap-icons/font/bootstrap-icons.css');
-  app.import('node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff', {
-    destDir: 'assets/fonts',
-  });
-  app.import('node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2', {
-    destDir: 'assets/fonts',
-  });
+  app.import('node_modules/bootstrap/dist/css/bootstrap.min.css');
 
   return app.toTree();
 };
