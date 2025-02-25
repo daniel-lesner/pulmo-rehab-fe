106bde9c13b7858675542256491853147482690f
Daniel Lesner <daniel.lesner@icloud.com>
2025-02-25 09:45:41 +0200

    Text changes and reset dashboard variables

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,3 +1,16 @@
 import AuthenticatedRoute from './authenticated';
 
-export default class DashboardRoute extends AuthenticatedRoute {}
+export default class DashboardRoute extends AuthenticatedRoute {
+  resetController(controller, isExiting) {
+    if (isExiting)
+      controller.setProperties({
+        bracelet: null,
+        braceletId: null,
+        data: null,
+        isDataLoading: false,
+        selectedDataType: 'stats',
+        selectedTimeIntervalInMinutes: 60,
+        selectedDate: new Date().toISOString().split('T')[0],
+      });
+  }
+}
